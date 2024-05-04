4432efd09beeb683f49f72513814a4a5e68d0584
Hamza <hamza3zekri@gmail.com>
2024-05-04 22:50:56 +0100

    Setup Secretary and Admin model

--- src/Models/Admin.java
+++ src/Models/Admin.java
@@ -1,4 +1,72 @@
 package Models;
 
 public class Admin {
+
+    public Admin(int id, String cin, String firstName, String lastName, String email, String phone){
+        super(id,cin,firstName,lastName,email,phone);
+    }
+
+    public static Admin get(int id) {
+        String query = "SELECT * FROM admins WHERE admin_id = ?";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+            if(result.next()){
+                return new Admin(
+                        result.getInt("admin_id"),
+                        result.getString("cin"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("email"),
+                        result.getString("tele")
+                );
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void create(String cin, String firstName, String lastName, String email, String phone) {
+        String query = "INSERT INTO secretaries(cin, first_name, last_name, email, tele,login_id) VALUES (?, ?, ?, ?, ?,null)";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, cin);
+            preparedStatement.setString(2, firstName);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setString(4, email);
+            preparedStatement.setString(5, phone);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    public static void update(int id, String cin, String firstName, String lastName, String email, String phone) {
+        String query = "UPDATE secretaries SET cin=?,first_name = ?, last_name = ?, email = ?, tele = ? WHERE secretary_id=?";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, cin);
+            preparedStatement.setString(2, firstName);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setString(4, email);
+            preparedStatement.setString(5, phone);
+            preparedStatement.setInt(6, id);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/Models/Secretary.java
+++ src/Models/Secretary.java
@@ -4,19 +4,59 @@ import Database.DBConnection;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
-public class Secretary {
+public class Secretary extends Person{
 
-
-    public static ResultSet all() {
+    public Secretary(int id, String cin, String firstName, String lastName, String email, String phone){
+        super(id,cin,firstName,lastName,email,phone);
+    }
+    public static ArrayList<Secretary> all() {
         String query = "SELECT * FROM secretaries";
         try {
             Connection connection = DBConnection.getConnection();
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(query);
+            ArrayList<Secretary> secretaries = new ArrayList<>();
+            while(result.next()){
+                secretaries.add(new Secretary(
+                        result.getInt("secretary_id"),
+                        result.getString("cin"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("email"),
+                        result.getString("tele")
+                ));
+            }
             statement.close();
             connection.close();
-            return result;
+            return secretaries;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static Secretary get(int id) {
+        String query = "SELECT * FROM secretaries WHERE secretary_id = ?";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+            if(result.next()){
+                return new Secretary(
+                        result.getInt("secretary_id"),
+                        result.getString("cin"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("email"),
+                        result.getString("tele")
+                );
+            }
+            statement.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -42,7 +82,7 @@ public class Secretary {
     }
 
     public static void delete(int id) {
-        String query = "DELETE FROM secretaries WHERE id = ?";
+        String query = "DELETE FROM secretaries WHERE secretary_id = ?";
         try {
             Connection connection = DBConnection.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -56,7 +96,7 @@ public class Secretary {
     }
 
     public static void update(int id, String cin, String firstName, String lastName, String email, String phone) {
-        String query = "UPDATE secretaries SET cin=?,first_name = ?, last_name = ?, email = ?, tele = ? WHERE id=?";
+        String query = "UPDATE secretaries SET cin=?,first_name = ?, last_name = ?, email = ?, tele = ? WHERE secretary_id=?";
         try {
             Connection connection = DBConnection.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -75,20 +115,6 @@ public class Secretary {
     }
 
 
-    public static ResultSet get(int id) {
-        String query = "SELECT * FROM secretaries WHERE id = ?";
-        try {
-            Connection connection = DBConnection.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, id);
-            ResultSet result = statement.executeQuery();
-            statement.close();
-            connection.close();
-            return result;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+
 }
 
