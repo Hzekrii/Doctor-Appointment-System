317ff210572ddfc93d750d198fe20ef318707e89
AbdallahRdf <abdallahradfi@gmail.com>
2024-05-07 13:00:16 +0100

    renamed some attributes in the Login.java

--- src/Views/Login.java
+++ src/Views/Login.java
@@ -1,7 +1,21 @@
 package Views;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
+import javax.swing.GroupLayout;
+import javax.swing.LayoutStyle;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 public class Login extends JFrame {
@@ -12,11 +26,11 @@ public class Login extends JFrame {
     private JPanel Left;
     private JPanel Right;
     private JButton loginButton;
-    private JLabel jLabel1;
-    private JLabel jLabel2;
-    private JLabel jLabel3;
+    private JLabel loginTitle;
+    private JLabel usernameTitle;
+    private JLabel passwordTitle;
     private JLabel jLabel5;
-    private JLabel jLabel6;
+    private JLabel leftBigTitle;
     private JLabel jLabel7;
     private JPanel jPanel1;
     private JPasswordField jPasswordField1;
@@ -32,11 +46,11 @@ public class Login extends JFrame {
         Right = new JPanel();
         Left = new JPanel();
         JLabel logoLabel = new JLabel();
-        jLabel1 = new JLabel();
-        jLabel2 = new JLabel();
-        jLabel3 = new JLabel();
+        loginTitle = new JLabel();
+        usernameTitle = new JLabel();
+        passwordTitle = new JLabel();
         jLabel5 = new JLabel();
-        jLabel6 = new JLabel();
+        leftBigTitle = new JLabel();
         jLabel7 = new JLabel();
         jTextField1 = new JTextField();
         jPasswordField1 = new JPasswordField();
@@ -47,7 +61,7 @@ public class Login extends JFrame {
         Image scaledLogo = logoImage.getScaledInstance(110, 110,  java.awt.Image.SCALE_SMOOTH); // Resize the image to 100x100 with anti-aliasing
         logoLabel.setIcon(new ImageIcon(scaledLogo));
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("LOGIN");
         setPreferredSize(new Dimension(1240, 600)); // Updated size
         setResizable(false);
@@ -60,9 +74,9 @@ public class Login extends JFrame {
 
         Right.add(logoLabel);
 
-        jLabel6.setFont(new Font("Showcard Gothic", 1, 36)); // Updated font size
-        jLabel6.setForeground(new Color(255, 255, 255));
-        jLabel6.setText("Hospital Appointment System");
+        leftBigTitle.setFont(new Font("Showcard Gothic", 1, 36)); // Updated font size
+        leftBigTitle.setForeground(new Color(255, 255, 255));
+        leftBigTitle.setText("Hospital Appointment System");
 
         jLabel7.setFont(new Font("Segoe UI Light", 0, 14)); // NOI18N
         jLabel7.setForeground(new Color(204, 204, 204));
@@ -84,7 +98,7 @@ public class Login extends JFrame {
                                 .addContainerGap(180, Short.MAX_VALUE)) // Adjust horizontal padding as needed
                         .addGroup(RightLayout.createSequentialGroup()
                                 .addGap(103, 103, 103)
-                                .addComponent(jLabel6))
+                                .addComponent(leftBigTitle))
         );
 
         RightLayout.setVerticalGroup(
@@ -95,7 +109,7 @@ public class Login extends JFrame {
                                         .addComponent(jLabel5)
                                         .addComponent(logoLabel))
                                 .addGap(40, 40, 40) // Adjust vertical spacing between logo and other elements as needed
-                                .addComponent(jLabel6)
+                                .addComponent(leftBigTitle)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
                                 .addComponent(jLabel7)
                                 .addGap(78, 78, 78))
@@ -105,20 +119,20 @@ public class Login extends JFrame {
 
         Left.setBackground(new Color(255, 255, 255));
 
-        jLabel1.setFont(new Font("Segoe UI", 1, 36)); // NOI18N
-        jLabel1.setForeground(new Color(0, 102, 102));
-        jLabel1.setText("LOGIN");
+        loginTitle.setFont(new Font("Segoe UI", 1, 36)); // NOI18N
+        loginTitle.setForeground(new Color(0, 102, 102));
+        loginTitle.setText("LOGIN");
 
-        jLabel2.setFont(new Font("Segoe UI", Font.BOLD, 14)); // NOI18N
-        jLabel2.setText("Username");
+        usernameTitle.setFont(new Font("Segoe UI", Font.BOLD, 14)); // NOI18N
+        usernameTitle.setText("Username");
 
         jTextField1.setFont(new Font("Segoe UI", 0, 16)); // NOI18N
         jTextField1.setForeground(new Color(102, 102, 102));
 
         jPasswordField1.setFont(new Font(null, 0, 16));
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", Font.BOLD, 14)); // NOI18N
-        jLabel3.setText("Password");
+        passwordTitle.setFont(new java.awt.Font("Segoe UI", Font.BOLD, 14)); // NOI18N
+        passwordTitle.setText("Password");
 
         // Initialize the JComboBox with roles
         String[] roles = {"Secretary", "Admin"};
@@ -141,13 +155,13 @@ public class Login extends JFrame {
                                 .addGroup(LeftLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addGroup(LeftLayout.createSequentialGroup()
                                                 .addGap(138, 138, 138)
-                                                .addComponent(jLabel1))
+                                                .addComponent(loginTitle))
                                         .addGroup(LeftLayout.createSequentialGroup()
                                                 .addGap(30, 30, 30)
                                                 .addGroup(LeftLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                                        .addComponent(jLabel2)
+                                                        .addComponent(usernameTitle)
                                                         .addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 343, GroupLayout.PREFERRED_SIZE)
-                                                        .addComponent(jLabel3)
+                                                        .addComponent(passwordTitle)
                                                         .addComponent(jPasswordField1, GroupLayout.PREFERRED_SIZE, 343, GroupLayout.PREFERRED_SIZE)
                                                         .addGroup(LeftLayout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
                                                                 .addComponent(roleComboBox, GroupLayout.Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -158,13 +172,13 @@ public class Login extends JFrame {
                 LeftLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(LeftLayout.createSequentialGroup()
                                 .addGap(51, 51, 51)
-                                .addComponent(jLabel1)
+                                .addComponent(loginTitle)
                                 .addGap(40, 40, 40)
-                                .addComponent(jLabel2)
+                                .addComponent(usernameTitle)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(jLabel3)
+                                .addComponent(passwordTitle)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jPasswordField1, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
                                 .addGap(26, 26, 26)
