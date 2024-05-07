b4a43290baa4e3e5f32d36c8109189be8ab46215
IDOUFKIR Kamal <idoufkir.kamal@gmail.com>
2024-05-07 23:28:22 +0100

    Add the Components for Admin

---
+++ src/Views/Admin/component/Menu.java
@@ -0,0 +1,131 @@
+package Views.Admin.component;
+
+import Views.Admin.event.EventMenuSelected;
+import Views.Admin.model.Model_Menu;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class Menu extends javax.swing.JPanel {
+
+    private EventMenuSelected event;
+
+    public void addEventMenuSelected(EventMenuSelected event) {
+        this.event = event;
+        listMenu1.addEventMenuSelected(event);
+    }
+
+    public Menu() {
+        initComponents();
+        listMenu1.setOpaque(false);
+        init();
+    }
+
+    private void init() {
+        listMenu1.addItem(new Model_Menu("1", "Dashboard", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("2", "Secretaries", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("3", "Doctors", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("4", "Patients", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("5", "Appointments", Model_Menu.MenuType.MENU));
+        listMenu1.addItem(new Model_Menu("6", "Logout", Model_Menu.MenuType.MENU));
+
+        listMenu1.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelMoving = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        listMenu1 = new Views.Admin.swing.ListMenu<>();
+
+        panelMoving.setOpaque(false);
+
+        // Update the logo size to 40x40
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/icons/logo.png"))); // NOI18N
+        javax.swing.ImageIcon icon = (javax.swing.ImageIcon) jLabel1.getIcon();
+        java.awt.Image img = icon.getImage();
+        java.awt.Image newImg = img.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
+        jLabel1.setIcon(new javax.swing.ImageIcon(newImg));
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        jLabel1.setForeground(new Color(0, 102, 102));
+        jLabel1.setText("Hospital Appointment System");
+
+        javax.swing.GroupLayout panelMovingLayout = new javax.swing.GroupLayout(panelMoving);
+        panelMoving.setLayout(panelMovingLayout);
+        panelMovingLayout.setHorizontalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelMovingLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panelMovingLayout.setVerticalGroup(
+            panelMovingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMovingLayout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelMoving, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panelMoving, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    protected void paintChildren(Graphics grphcs) {
+        super.paintComponent(grphcs);
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint g = new GradientPaint(0, 0, Color.decode("#13A4A4"), 0, getHeight(), Color.decode("#006666"));
+        g2.setPaint(g);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        g2.dispose(); // Dispose graphics context to free resources
+
+        // Paint the components after the background
+        super.paintChildren(grphcs);
+    }
+
+    private int x;
+    private int y;
+
+    public void initMoving(JFrame fram) {
+        panelMoving.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                x = me.getX();
+                y = me.getY();
+            }
+
+        });
+        panelMoving.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent me) {
+                fram.setLocation(me.getXOnScreen() - x, me.getYOnScreen() - y);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private Views.Admin.swing.ListMenu<String> listMenu1;
+    private javax.swing.JPanel panelMoving;
+    // End of variables declaration//GEN-END:variables
+}
