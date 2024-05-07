d889237866872a30e0c16a2b1a8bbee9d48f8a1b
Hamza <hamza3zekri@gmail.com>
2024-05-07 21:07:21 +0100

    Setup AdminController

--- src/Controllers/AdminController.java
+++ src/Controllers/AdminController.java
@@ -1,4 +1,20 @@
 package Controllers;
 
+import Models.Admin;
+
+import java.util.ArrayList;
+
 public class AdminController {
+    private AdminController(){
+
+    }
+    // Admin
+    public static Admin getAdmin(int id) {
+        // Call the model method to retrieve a secretary by ID
+        return Admin.get(id);
+    }
+    public static void updateAdmin(int id, String cin, String firstName, String lastName, String email, String phone) {
+        Admin.update(id, cin, firstName, lastName, email, phone);
+    }
+
 }
--- src/Controllers/SecretaryController.java
+++ src/Controllers/SecretaryController.java
@@ -14,40 +14,20 @@ public class SecretaryController {
 
     public static ArrayList<Secretary> getAllSecretaries() {
         // Call the model method to retrieve all secretaries
-        ArrayList<Secretary> secretaries = Secretary.all();
-        if (secretaries != null) {
-            return secretaries;
-        } else {
-            // Handle the case when the list is null (e.g., log, display error message)
-            JOptionPane.showMessageDialog(null, "Failed to retrieve secretaries from the database.", "Error", JOptionPane.ERROR_MESSAGE);
-            return null;
-        }
+        return Secretary.all();
     }
 
     public static Secretary getSecretary(int id) {
         // Call the model method to retrieve a secretary by ID
-        Secretary secretary = Secretary.get(id);
-        if (secretary != null) {
-            return secretary;
-        } else {
-            JOptionPane.showMessageDialog(null, "Failed to retrieve secretary with ID: " + id, "Error", JOptionPane.ERROR_MESSAGE);
-            return null;
-        }
+        return Secretary.get(id);
     }
 
     public static void createSecretary(String cin, String firstName, String lastName, String email, String phone) {
-       // try {
-            // Call the model method to create a new secretary
             Secretary.create(cin, firstName, lastName, email, phone);
-       // } catch (SQLException e) {
-            // Handle the exception
-           // JOptionPane.showMessageDialog(null, "Failed to create secretary: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-      //  }
     }
 
     public static void updateSecretary(int id, String cin, String firstName, String lastName, String email, String phone) {
         Secretary.update(id, cin, firstName, lastName, email, phone);
-        //JOptionPane.showMessageDialog(null, "Secretary updated successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void deleteSecretary(int id) {
