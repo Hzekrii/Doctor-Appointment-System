767d931d8e6192ea4bb4bb67b69a56f243f951e4
Hamza <hamza3zekri@gmail.com>
2024-05-09 02:46:29 +0100

    setup add form of appointment

--- src/Views/Secretary/pages/AddNewAppointment.java
+++ src/Views/Secretary/pages/AddNewAppointment.java
@@ -1,13 +1,28 @@
 package Views.Secretary.pages;
 
+import Controllers.AppointmentController;
+import Controllers.DoctorController;
+import Controllers.PatientController;
+import Models.Appointment;
+import Models.Doctor;
+import Models.Patient;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class AddNewAppointment extends JFrame {
     private JComboBox<String> doctorComboBox;
-    private JComboBox<String> appointmentComboBox;
-    private JComboBox<String> roomComboBox;
+    private JComboBox<String> patientComboBox;
+    private JComboBox<Appointment.Room> roomComboBox;
     private JTextField dateField;
     private JTextField timeField;
 
@@ -19,14 +34,37 @@ public class AddNewAppointment extends JFrame {
     private JLabel dateLabel;
     private JLabel timeLabel;
     private JLabel roomLabel;
-
-    private String[] patients = {"patient1", "patient2"};
-    private String[] doctors = {"doctor1", "doctor2"};
-    private String[] rooms = {"room1", "room2"};
+    private ArrayList<Patient> patients;
+    private ArrayList<Doctor> doctors;
+    private String[] doctorNames;
+    private String[] patientNames;
+    private Appointment.Room[] rooms;
 
     public AddNewAppointment() {
+        rooms= Appointment.Room.values();
+       // rooms = addDefaultRoomOption(rooms);
+        patients= PatientController.getPatients();
+        doctors= DoctorController.getDoctors();
+        doctorNames=new String[doctors.size()];
+        patientNames= new String[patients.size()];
         initComponents();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set close operation
+        initAddAppointmentButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get data from user input fields
+                int doctor_id =getDoctor();
+                int patient_id=getPatient();
+                Date date = getDate();
+                Time time = getTime();
+                Appointment.Room room = getRoom();
+
+                // Call the appointmentController to create an appointment
+                AppointmentController.createAppointment(patient_id, doctor_id, date, time, Appointment.AppointmentStatus.SCHEDULED ,room);
+
+                // Optionally, you can perform additional actions after creating the appointment
+            }
+        });
     }
 
     private void initComponents() {
@@ -38,8 +76,20 @@ public class AddNewAppointment extends JFrame {
         dateLabel = new JLabel();
         timeLabel = new JLabel();
         roomLabel = new JLabel();
-        doctorComboBox = new JComboBox<>(doctors);
-        appointmentComboBox = new JComboBox<>(patients);
+        int index=0;
+        for(Patient patient: patients){
+           String fullName= patient.getFirstName()+" "+patient.getLastName();
+           patientNames[index++]=fullName;
+        }
+        index=0;
+        for(Doctor doctor: doctors){
+            String fullName= doctor.getFirstName()+" "+doctor.getLastName();
+            doctorNames[index++]=fullName;
+        }
+       // doctorNames = addDefaultDoctorOption(doctorNames);
+       // patientNames = addDefaultPatientOption(patientNames);
+        doctorComboBox = new JComboBox<>(doctorNames);
+        patientComboBox = new JComboBox<>(patientNames);
         roomComboBox = new JComboBox<>(rooms);
         dateField = new JTextField();
         timeField = new JTextField();
@@ -71,9 +121,9 @@ public class AddNewAppointment extends JFrame {
         patientLabel.setFont(new Font("Segoe UI", Font.BOLD, 14));
         patientLabel.setText("Patient");
 
-        appointmentComboBox.setPreferredSize(new Dimension(150, 30));
-        appointmentComboBox.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-        appointmentComboBox.setBackground(Color.white);
+        patientComboBox.setPreferredSize(new Dimension(150, 30));
+        patientComboBox.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+        patientComboBox.setBackground(Color.white);
 
         dateLabel.setFont(new Font("Segoe UI", Font.BOLD, 14));
         dateLabel.setText("Date");
@@ -115,7 +165,7 @@ public class AddNewAppointment extends JFrame {
                                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                                 .addGroup(PanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                                         .addComponent(doctorComboBox)
-                                                        .addComponent(appointmentComboBox)
+                                                        .addComponent(patientComboBox)
                                                         .addComponent(dateField)
                                                         .addComponent(timeField)
                                                         .addComponent(roomComboBox, 0, 300, Short.MAX_VALUE))
@@ -136,7 +186,7 @@ public class AddNewAppointment extends JFrame {
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(PanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(patientLabel)
-                                        .addComponent(appointmentComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(patientComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(PanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(dateLabel)
@@ -174,28 +224,93 @@ public class AddNewAppointment extends JFrame {
 
         getAccessibleContext().setAccessibleName("ADDAPPOINTMENT");
 
+        // Set default selected items in doctorComboBox , patientComboBox and roomComboBox
+        doctorComboBox.setSelectedIndex(0);
+        patientComboBox.setSelectedIndex(0);
+        roomComboBox.setSelectedIndex(0);
+
         pack();
         setVisible(true);
     }
 
-    public String getDoctor() {
-        return (String) doctorComboBox.getSelectedItem();
+    // Method to add a default "Select" option to the beginning of the array
+    private String[] addDefaultOption(String[] array, String defaultOption) {
+        String[] arrayWithDefault = new String[array.length + 1];
+        arrayWithDefault[0] = defaultOption; // Default option
+        System.arraycopy(array, 0, arrayWithDefault, 1, array.length);
+        return arrayWithDefault;
     }
 
-    public String getAppointment() {
-        return (String) appointmentComboBox.getSelectedItem();
+  /*  // Method to add a default "Select Doctor" option to the beginning of the doctor names array
+    private String[] addDefaultDoctorOption(String[] array) {
+        return addDefaultOption(array, "...");
     }
 
-    public String getDate() {
-        return dateField.getText();
+    // Method to add a default "Select Patient" option to the beginning of the patient names array
+    private String[] addDefaultPatientOption(String[] array) {
+        return addDefaultOption(array, "...");
     }
 
-    public String getTime() {
-        return timeField.getText();
+    // Method to add a default "Select Room" option to the beginning of the Room enum array
+    private Appointment.Room[] addDefaultRoomOption(Appointment.Room[] rooms) {
+        Appointment.Room[] roomsWithDefault = new Appointment.Room[rooms.length + 1];
+        roomsWithDefault[0] = null; // Default option
+        System.arraycopy(rooms, 0, roomsWithDefault, 1, rooms.length);
+        return roomsWithDefault;
+    }*/
+
+
+    public int getDoctor() {
+        String doctor= (String) doctorComboBox.getSelectedItem();
+        for(Doctor d: doctors){
+            if(d.getFirstName()+" "+d.getLastName() == doctor){
+                return d.getID();
+            }
+        }
+        return -1;
     }
 
-    public String getRoom() {
-        return (String) roomComboBox.getSelectedItem();
+    public int getPatient() {
+        String patient= (String) patientComboBox.getSelectedItem();
+        for(Patient p: patients){
+            if(p.getFirstName()+" "+p.getLastName() == patient){
+                return p.getID();
+            }
+        }
+        return -1;
+    }
+
+    public Date getDate() {
+
+        String pattern = "yyyy-MM-dd";
+        // Create a SimpleDateFormat object with the desired pattern
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+            // Parse the string to obtain a Date object
+            return dateFormat.parse(dateField.getText());
+        }catch(ParseException e){
+            return null;
+        }
+
+    }
+
+    public Time getTime() {
+        // Define the time format pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // Parse the string to obtain a LocalTime object
+        LocalTime localTime = LocalTime.parse(timeField.getText(), formatter);
+        // Convert LocalTime to Time object
+        return Time.valueOf(localTime);
+    }
+
+    public Appointment.Room getRoom() {
+
+        for(Appointment.Room r : Appointment.Room.values()){
+            if(r.name().equalsIgnoreCase((String) roomComboBox.getSelectedItem())){
+                return r;
+            }
+        }
+        return null;
     }
 
     public void initAddAppointmentButtonActionListener(ActionListener listener) {
